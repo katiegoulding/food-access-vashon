03d882e3477a984de7713df1a1a02b8731f86213
antonzheng <anton.zheng@gmail.com>
2019-03-05 21:54:16 -0800

    Revert "logout and reroute on no login"
    This reverts commit 96e1ddffc6af414a63329234e803fcb518d96f32.

--- fapadmin/src/components/MainActivity.jsx
+++ fapadmin/src/components/MainActivity.jsx
@@ -1,35 +1,12 @@
 import React from "react";
 import { Link, Redirect } from "react-router-dom";
 import constants from "./constants";
-import firebase from 'firebase/app'
-import 'firebase/auth';
-import 'firebase/database';
+
 export default class MainActivity extends React.Component {
-
-    componentDidMount() {
-        this.authUnsub = firebase.auth().onAuthStateChanged(user => {
-            if (!user) {
-                this.props.history.push(constants.routes.logIn)
-            }
-        });
-    }
-
-    handleSignOut() {
-        firebase.auth().signOut()
-            .then(this.props.history.push(constants.routes.logIn))
-    };
-
     render() {
         return (
             <div>
                 <h1>Main Screen</h1>
-
-                <button
-                    type="submit"
-                    className="btn btn-danger col-1"
-                    onClick={() => this.handleSignOut()}>
-                    Sign Out
-                </button>
             </div >
         )
     }
