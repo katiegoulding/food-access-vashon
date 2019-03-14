85826e3832688b95c43f38f56676dcc53d0575ea
antonzheng <anton.zheng@gmail.com>
2019-03-14 16:11:29 -0700

    Updates

---
+++ precache-manifest.ed0289cc97c04ed9de316958843a58dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e70803cad549f0725d5",
+    "url": "/food-access-vashon/static/js/runtime~main.4e70803c.js"
+  },
+  {
+    "revision": "46ba00da72ff614eb542",
+    "url": "/food-access-vashon/static/js/main.46ba00da.chunk.js"
+  },
+  {
+    "revision": "dfc124ac119003202749",
+    "url": "/food-access-vashon/static/js/2.dfc124ac.chunk.js"
+  },
+  {
+    "revision": "46ba00da72ff614eb542",
+    "url": "/food-access-vashon/static/css/main.5a689a93.chunk.css"
+  },
+  {
+    "revision": "f603dc562f6339d78b8b5663662f7b7c",
+    "url": "/food-access-vashon/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c36c666c5e5c1fe86e9e2f0c912453cf.js"
+  "/food-access-vashon/precache-manifest.ed0289cc97c04ed9de316958843a58dd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/food-access-vashon/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
