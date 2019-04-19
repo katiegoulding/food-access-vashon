f6f0f234b3a75ebc2cb0f6d8f1b7b670a82e27b2
August Carow <august.carow@gmail.com>
2019-04-19 13:51:53 -0700

    add submit functionality for new buck set form

--- fapadmin/src/components/CreateBucks.jsx
+++ fapadmin/src/components/CreateBucks.jsx
@@ -5,6 +5,16 @@ export default class CreateBucks extends React.Component {
 
     constructor(props) {
         super(props)
+        this.state = {
+            doveCount: 0,
+            vyfsCount: 0,
+            lacomunidadCount: 0,
+            vashonhouseholdCount: 0
+        };
+    }
+
+    collectBuckCount(event) {
+
     }
 
     render() {
@@ -18,21 +28,24 @@ export default class CreateBucks extends React.Component {
                     <p>Organization Buck Counts</p>
                     <label>
                         Dove
-                        <input type="text" name="DOVE buck number" />
+                        <input type="text" name="DOVE buck number" placeholder="number of bucks"/>
                     </label>
+                    <br />
                     <label>
                         VYFS
-                        <input type="text" name="VYFS buck number" />
+                        <input type="text" name="VYFS buck number" placeholder="number of bucks"/>
                     </label>
+                    <br />
                     <label>
                         La Comunidad
-                        <input type="text" name="La Comunidad buck number" />
+                        <input type="text" name="La Comunidad buck number" placeholder="number of bucks"/>
                     </label>
+                    <br />
                     <label>
                         Vashon Household
-                        <input type="text" name="Vashon Household buck number" />
+                        <input type="text" name="Vashon Household buck number" placeholder="number of bucks"/>
                     </label>
-
+                    <br />
                     <input type="submit" value="Submit" />
                 </form>
             </div>
