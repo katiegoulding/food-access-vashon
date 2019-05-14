47342393071e12d8b179fd4c3c5dc2a92e45544c
August Carow <august.carow@gmail.com>
2019-05-14 10:46:23 -0700

    recover some scan work

--- fapadmin/src/components/Scan.jsx
+++ fapadmin/src/components/Scan.jsx
@@ -13,14 +13,76 @@ export default class Scan extends React.Component {
     };
   }
 
-  handleScan = data => {
-    data = JSON.parse(data);
-    if (data) {
-      this.setState({
-        result: data
-      });
+    handleScan = data => {
+
+        if (data) {
+            
+            // TODO: make sure that we have an id in the data var
+            let voucherRef = firebase.database().ref('vouchers/' + data)
+            let userRef = firebase.database().ref('users/' + this.props.userId)
+
+            console.log("this is the uid passed from props", this.props.userId)
+            console.log("this is the role passed from props", this.props.role)
+
+            if(this.props.role == "farmer") {
+                // TODO: fill in the corresponding data visualization object with a new scan
+                
+                // if they are a farmer, fill in the "whenRedeemed" field on voucher object to the current time
+                let redeemedOnRef = firebase.database().ref('vouchers/' + data + "/redeemedOn")
+                redeemedOnRef.on('value', function(snapshot) {
+                    if(snapshot.val() == "") {
+                        console.log("this voucher has not been redeemed before")
+                        voucherRef.update({
+                            redeemedOn: new String(new Date())
+                        })
+                    } else {
+                        console.log("this voucher has already been redeemed")
+                    }
+                });
+
+                // also add the voucher id to the user's list of associated ids
+                userRef.child("vouchersList").update({
+                    [data]: "true"
+                })
+
+            } else if(this.props.role == "caseworker") {
+                // TODO: fill in the corresponding data visualization object with a new scan
+
+                // if they are a caseworker, fill in the "handedOutOn" field on voucher object to the current time
+                let handedOutRef = firebase.database().ref('vouchers/' + data + "/handedOutOn")
+                handedOutRef.on('value', function(snapshot) {
+                    if(snapshot.val() == "") {
+                        console.log("this voucher has not been handed out before")
+                        voucherRef.update({
+                            handedOutOn: new String(new Date())
+                        })
+                    } else {
+                        console.log("this voucher has already been handed out")
+                    }
+                });
+
+                // also add the voucher id to the user's list of associated ids
+                userRef.child("vouchersList").update({
+                    [data]: "true"
+                })
+
+            } else { // not caseworker or farmer
+                // if they are an admin we could give them an informational message about the voucher? not required.
+                // just give an informational message?
+                // OR don't do anything? 
+
+            }
+            
+            // Make a call to a google cloud function
+            // URL TO relevant info
+            // GCF will take 
+
+            this.setState({
+                result: data,
+            })
+        }
     }
-  };
+
   handleError = err => {
     console.error(err);
     this.setState({
